983ab500227ea9ee49c0d4141e0de9af1d9477bd
edaclskn <141743590+edaclskn@users.noreply.github.com>
2023-08-18 16:18:25 +0300

    First commit

---
+++ main.py
@@ -0,0 +1,33 @@
+name = "Eda"
+surname = "Çalışkan"
+
+surnames = {"Yıldız", "Aksoy", "Demir"}
+
+if surname in surnames:
+    print(True)
+elif surnames not in surnames:
+    print(False)
+else:
+    print(False)
+
+
+for soyad in surnames:
+    #print(soyad)
+    if surname == soyad:
+        print(True)
+        print(soyad)
+    else:
+        print(False)
+        print(soyad)   
+a=0
+while 1:
+print(Elma)
+print(a)
+a+=1
+
+print(name[0])
+
+print(surname[0])
+
+
+print("")
